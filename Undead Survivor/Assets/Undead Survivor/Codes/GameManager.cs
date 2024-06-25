e42381a455370259d4e1827deb61c0f8e90ff59a
BJY029 <go333dragon@gmail.com>
2024-06-25 14:44:39 +0900

    몬스터 데이터 설정

--- Undead Survivor/Assets/Undead Survivor/Codes/GameManager.cs
+++ Undead Survivor/Assets/Undead Survivor/Codes/GameManager.cs
@@ -5,11 +5,26 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+
+	//레벨 디자인을 위한 변수 선언
+	public float gameTime;
+	public float maxGameTime = 2 * 10f;
+
     public Player player;
 	public PoolManager pool;
 
 	private void Awake()
 	{
 		instance = this;
+	}
+
+	//시간을 관리하는 함수
+	private void Update()
+	{
+		gameTime += Time.deltaTime;
+
+		//gameTime이 maxGameTime을 넘어가지 않도록 설정
+		if(gameTime > maxGameTime)
+			gameTime = maxGameTime;
 	}
 }
--- Undead Survivor/Assets/Undead Survivor/Codes/Spawner.cs
+++ Undead Survivor/Assets/Undead Survivor/Codes/Spawner.cs
@@ -1,12 +1,16 @@
 using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
 	//스폰할 포인터를 저장할 배열 선언
     public Transform[] spawnPoint;
+	public SpawnData[] spawnDatas;
+
+	int level;
 
 	//스폰 타이밍을 정하기 위해 선언
     float timer;
@@ -25,8 +29,13 @@ public class Spawner : MonoBehaviour
 		//매 프레임의 시간을 더한다.
 		timer += Time.deltaTime;
 
+		//FloorToInt : 소수점 아래는 버리고 int 형으로 바꾸는 함수,
+		//CeilToInt : 소수점 아래를 올리고 int 형으로 바꾸는 함수
+		//10초가 지날때마다, 레벨이 1씩 증가한다.
+		level = Mathf.Min(Mathf.FloorToInt(GameManager.instance.gameTime / 10f), spawnDatas.Length - 1);
+
 		//시간이 0.2초가 지나면
-		if(timer > 0.2f)
+		if (timer > spawnDatas[level].spawnTime) 
 		{
 			//타이머를 0으로 초기화한다.
 			timer = 0f;
@@ -40,11 +49,24 @@ public class Spawner : MonoBehaviour
 	{
 		//우선 생성할 몬스터를 정한다.
 		//풀에 저장된 몬스터들을 랜덤하게 가져온다.
-		GameObject enemy =  GameManager.instance.pool.Get(Random.Range(0,2));
+		GameObject enemy =  GameManager.instance.pool.Get(0);
 
 		//그 후, 몬스터를 랜덤한 위치에 생성한다.
 		//위치는 spawnPoint에 저장된 각 포인터중 하나를 랜덤하게 선출해서 가져온다.
 		//자기자신이 0에 저장되있기 때문에, Range는 1부터 시작된다.
 		enemy.transform.position = spawnPoint[Random.Range(1, spawnPoint.Length)].position;
+
+		enemy.GetComponent<Enemy>().Init(spawnDatas[level]);
 	}
+}
+
+//직렬화(Serialization) : 개체를 저장 혹은 전송하기 위해 변환
+//unity에서의 인스펙터 창에서도 관리할 수 있게 하기 위해 직렬화를 진행한다.
+[System.Serializable]
+public class SpawnData
+{
+	public int spriteType;
+	public float spawnTime;
+	public int health;
+	public float speed;
 }
